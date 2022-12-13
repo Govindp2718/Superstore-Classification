ff77ef57256a8354a60b721d773eca050b54c2ac
Govind Pande <117948813+Govindp2718@users.noreply.github.com>
2022-12-12 22:58:59 -0500

    Update store.py

--- store.py
+++ store.py
@@ -153,8 +153,7 @@ def main():
                                          
     if classifier == "Decision Tree Classification':
         st.sidebar.subheader("Model Hyperparameters")
-        #n_estimators = st.sidebar.number_input("This is the number of trees in the forest", 100, 5000, step=10,
-                                               key='n_estimators')
+        #n_estimators = st.sidebar.number_input("This is the number of trees in the forest", 100, 5000, step=10,key='n_estimators')
         max_depth = st.sidebar.number_input("The maximum depth of the tree", 1, 100,5, step=2, key='max_depth')
         #bootstrap = st.sidebar.radio("Bootstrap samples when building trees", ("True", "False"), key='bootstrap')
         metrics = st.sidebar.multiselect("What matrix to plot?", ("Confusion Matrix", "ROC Curve",
